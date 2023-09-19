8883e3f95c35162284d51067868f67f13ded99f1
raflisalam <raflisalam.dokumen@gmail.com>
2023-09-19 09:59:19 +0800

    Fix:(WelcomeScreen) from overflow

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -51,66 +51,79 @@ class WelcomeScreen extends StatelessWidget {
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height / 2.666,
-                padding: EdgeInsets.only(top: 30, bottom: 30),
+                decoration: BoxDecoration(
+                  color: primaryColor,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2.666,
                 decoration: BoxDecoration(
                   color: backgroundColor,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(70),
                   ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                child: ListView(
+                  padding: EdgeInsets.symmetric(vertical: 30),
                   children: [
-                    Text(
-                      "Find Perfect Course",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Poppins',
-                        color: primaryVariantColor,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1,
-                        wordSpacing: 2,
+                    Center(
+                      child: Text(
+                        "Find Perfect Course",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Poppins',
+                          color: primaryVariantColor,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1,
+                          wordSpacing: 2,
+                        ),
                       ),
                     ),
                     SizedBox(height: 15),
                     Padding(
                       padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Text(
-                        "We will teach you to really understand and get work-ready skills for your future career",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 14,
-                          fontWeight: FontWeight.w100,
-                          color: primaryVariantColor,
+                      child: Center(
+                        child: Text(
+                          "We will teach you to really understand and get work-ready skills for your future career",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w100,
+                            color: primaryVariantColor,
+                          ),
                         ),
                       ),
                     ),
                     SizedBox(height: 20),
-                    Material(
-                      color: primaryVariantColor,
-                      borderRadius: BorderRadius.circular(10),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => HomeScreen(),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 70),
-                          child: Text(
-                            "Let's Learn",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 1,
+                    Center(
+                      child: Material(
+                        color: primaryVariantColor,
+                        borderRadius: BorderRadius.circular(10),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => HomeScreen(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 70),
+                            child: Text(
+                              "Let's Learn",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
                             ),
                           ),
                         ),
